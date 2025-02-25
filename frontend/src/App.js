b62ce2c6b977c4d4ba1691c50465d25f97f3ed3a
vikash kumar <vikashkumar24041997@gmail.com>
2025-02-26 00:21:47 +0530

    test1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,17 @@
-function App() {
-  return <h1>Welcome to proshop</h1>;
-}
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 export default App;
