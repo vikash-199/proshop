024db996013247c21f8e06c29b6ce73888c0e2b6
vikash kumar <vikashkumar24041997@gmail.com>
2025-02-27 23:04:18 +0530

    test

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
 import products from "../products";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
   const product = products.find((p) => p._id === productId);
-  return <div>ProductScreen</div>;
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
