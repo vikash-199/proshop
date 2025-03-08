b3261566e2867b62a480dd8799667917218b53d8
vikash kumar <vikashkumar24041997@gmail.com>
2025-03-08 22:59:42 +0530

    test

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -29,9 +29,40 @@ const ProductScreen = () => {
               />
             </ListGroup.Item>
             <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md={3}></Col>
+        <Col md={3}>
+          <Card variant="flush">
+            <ListGroup.Item>
+              <Row>
+                <Col>Price:</Col>
+                <Col>
+                  <strong>${product.price}</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Status:</Col>
+                <Col>
+                  <strong>
+                    ${product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                className="btn-block"
+                type="button"
+                disabled={product.countInStock === 0}
+              >
+                Add To Cart
+              </Button>
+            </ListGroup.Item>
+          </Card>
+        </Col>
       </Row>
     </>
   );
