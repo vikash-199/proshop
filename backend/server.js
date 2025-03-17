f0f21ff26c0295bfef25166dfcf25fe55bccaa86
vikash kumar <vikashkumar24041997@gmail.com>
2025-03-18 01:45:20 +0530

    testing backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
