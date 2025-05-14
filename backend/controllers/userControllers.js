96b7b195ebadc9302b6ab2b5f3fee125cf3ded5c
vikash kumar <vikashkumar24041997@gmail.com>
2025-05-14 15:02:43 +0530

    jwt authenticatin

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,104 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+import User from "../models/userModel.js";
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    // Set JWT as HTTP-Only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 60 * 24 * 60 * 60 * 1000, // 60 days
+    });
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+// @desc    Auth user & get token
+// @route   POST /api/users/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("register User");
+});
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/login
+// @access  Private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("logout User");
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("get User profile");
+});
+
+// @desc    update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("update User profile");
+});
+
+// @desc    Get users
+// @route   GET /api/users
+// @access  Private / Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get Users");
+});
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private / Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get User by ID");
+});
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private / Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete User");
+});
+
+// @desc    update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("update User ");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,31 @@ dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json()); //This middleware parses incoming JSON data in the request body and makes it available as a JavaScript object in req.body.
+app.use(express.urlencoded({ extended: true })); //This middleware parses URL-encoded data (like form submissions using <form>) and also puts it into req.body.
+
+/*
+In modern apps, it's common to use both, especially when:
+
+You accept both JSON requests (from frontend or API clients)
+
+And form submissions (from classic HTML forms)
+*/
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
