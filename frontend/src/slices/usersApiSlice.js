5daf5f0152c2c21e6a3beb5f35f6f4862bce6238
vikash kumar <vikashkumar24041997@gmail.com>
2025-05-17 16:34:08 +0530

    usersApiSlice implement in redux

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,17 @@
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice"; // dealing with async request
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        // eslint-disable-next-line no-undef
+        url: USERS_URL / auth,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
