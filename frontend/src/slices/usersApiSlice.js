9c79a8fe5f3342603ed40baf5c4cc308574d6477
vikash kumar <vikashkumar24041997@gmail.com>
2025-05-18 15:29:17 +0530

    frontent auth

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -6,7 +6,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     login: builder.mutation({
       query: (data) => ({
         // eslint-disable-next-line no-undef
-        url: USERS_URL / auth,
+        url: `${USERS_URL}/auth`,
         method: "POST",
         body: data,
       }),
