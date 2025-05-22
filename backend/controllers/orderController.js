577e34cdf018e8dc46eee5687f307f83c63be522
vikash kumar <vikashkumar24041997@gmail.com>
2025-05-23 00:39:43 +0530

    paypal implementation

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -71,7 +71,6 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("update order to paid");
   //   // NOTE: here we need to verify the payment was made to PayPal before marking
   //   // the order as paid
   //   const { verified, value } = await verifyPayPalPayment(req.body.id);
@@ -81,33 +80,33 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   //   const isNewTransaction = await checkIfNewTransaction(Order, req.body.id);
   //   if (!isNewTransaction) throw new Error("Transaction has been used before");
 
-  //   const order = await Order.findById(req.params.id);
+  const order = await Order.findById(req.params.id);
 
-  //   if (order) {
-  //     // check the correct amount was paid
-  //     const paidCorrectAmount = order.totalPrice.toString() === value;
-  //     if (!paidCorrectAmount) throw new Error("Incorrect amount paid");
+  if (order) {
+    // check the correct amount was paid
+    // const paidCorrectAmount = order.totalPrice.toString() === value;
+    // if (!paidCorrectAmount) throw new Error("Incorrect amount paid");
 
-  //     order.isPaid = true;
-  //     order.paidAt = Date.now();
-  //     order.paymentResult = {
-  //       id: req.body.id,
-  //       status: req.body.status,
-  //       update_time: req.body.update_time,
-  //       email_address: req.body.payer.email_address,
-  //     };
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
 
-  //     const updatedOrder = await order.save();
+    const updatedOrder = await order.save();
 
-  //     res.json(updatedOrder);
-  //   } else {
-  //     res.status(404);
-  //     throw new Error("Order not found");
-  //   }
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to delivered
-// @route   GET /api/orders/:id/deliver
+// @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send("update order to delevered");
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import { Provider } from "react-redux";
@@ -23,6 +24,7 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -37,6 +39,7 @@ const router = createBrowserRouter(
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
       </Route>
     </Route>
   )
@@ -46,7 +49,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
